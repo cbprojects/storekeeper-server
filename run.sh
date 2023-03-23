d853022c738d81a7c36c25bb6b67edec92a0cff3
Carlos Vera <46304171+cmvb@users.noreply.github.com>
2023-03-23 12:29:17 -0600

    Revisión Sonar fist time

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 # DEV
-export MONGO_HOST=agrofy-cluster-2.cluster-cqvwqfjurxih.us-east-1.docdb.amazonaws.com
-export MONGO_PORT=27017
-export PASSWORD_MONGO=dev03
-export USERNAME_MONGO=dev03
-export MONGO_DATABASE=gres-loan-orders-uat
+# export MONGO_HOST=agrofy-cluster-2.cluster-cqvwqfjurxih.us-east-1.docdb.amazonaws.com
+# export MONGO_PORT=27017
+# export PASSWORD_MONGO=dev03
+# export USERNAME_MONGO=dev03
+# export MONGO_DATABASE=gres-loan-orders-uat
+export MONGO_URL=mongodb+srv://storekeeper:OwGkje4clo7TJW18@cluster0.ygkvf6k.mongodb.net/storekeeper-qa?retryWrites=true&w=majority
 
 # QA
 
@@ -14,3 +15,4 @@ export MONGO_DATABASE=gres-loan-orders-uat
 # export PASSWORD_MONGO=qa03
 # export USERNAME_MONGO=qa03
 # export MONGO_DATABASE=gres-loan-orders-uat
+export MONGO_URL=mongodb+srv://storekeeper:OwGkje4clo7TJW18@cluster0.ygkvf6k.mongodb.net/storekeeper-qa?retryWrites=true&w=majority
