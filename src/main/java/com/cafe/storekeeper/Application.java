afa135d559b8e7876cecb6101d03606fe6f03f25
Carlos Vera <46304171+cmvb@users.noreply.github.com>
2023-04-04 22:28:41 -0600

    change startup

--- src/main/java/com/cafe/storekeeper/Application.java
+++ src/main/java/com/cafe/storekeeper/Application.java
@@ -2,6 +2,7 @@ package com.cafe.storekeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,10 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
 	}
 
 	@Bean
